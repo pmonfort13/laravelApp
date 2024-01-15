5f9d50a346885d0482b1ec446c299495dc8158d1
pmonfort13 <pmonfort@iesebre.com>
2024-01-15 19:19:33 +0100

    Sync

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,9 @@ class DatabaseSeeder extends Seeder
     {
         \App\Models\User::factory(5)->create();
 
+        \App\Models\Task::factory(7)->create();
+
+
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\PagesController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -28,5 +30,6 @@ use Illuminate\Support\Facades\Route;
 //});
 Route::get('/',[UserController::class,'index']);
 Route::get('/users',[UserController::class,'index']);
+Route::get('/tasks',[App\Http\Controllers\TaskController::class,'index']);
 Route::get('/contact',[PagesController::class,'contact']);
 Route::get('/about',[PagesController::class,'about']);
